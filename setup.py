ac9809bc1cb62ffee81a639d9eef245373e93bcc
rxl <me@ryanshea.org>
2013-12-21 18:35:52 -0500

    convert indentation to spaces

--- setup.py
+++ setup.py
@@ -7,24 +7,24 @@ Coins
 from setuptools import setup
 
 setup(
-	name='Coins',
-	version='0.1.3',
-	url='https://github.com/halfmoonlabs/coins',
-	license='MIT',
-	author='Halfmoon Labs',
-	author_email='hello@halfmoon.io',
-	description='Wallet interface for popular cryptocurrencies, including Bitcoin, Litecoin, Namecoin, Peercoin, and Primecoin.',
-	packages=[
-		'coins',
-	],
-	zip_safe=False,
-	install_requires=[
-		'ecdsa>=0.10',
-	],
-	classifiers=[
-		'Intended Audience :: Developers',
-		'License :: OSI Approved :: MIT License',
-		'Operating System :: OS Independent',
-		'Programming Language :: Python',
-	],
+    name='Coins',
+    version='0.1.3',
+    url='https://github.com/halfmoonlabs/coins',
+    license='MIT',
+    author='Halfmoon Labs',
+    author_email='hello@halfmoon.io',
+    description='Wallet interface for popular cryptocurrencies, including Bitcoin, Litecoin, Namecoin, Peercoin, and Primecoin.',
+    packages=[
+        'coins',
+    ],
+    zip_safe=False,
+    install_requires=[
+        'ecdsa>=0.10',
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
 )
