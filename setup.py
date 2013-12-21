83654ce48ad7ae885967131b2d39f958c454ecef
rxl <me@ryanshea.org>
2013-12-21 14:52:35 -0500

    remove wsgiref from requirements

--- setup.py
+++ setup.py
@@ -15,17 +15,16 @@ setup(
 	author_email='hello@halfmoon.io',
 	description='Wallet interface for popular cryptocurrencies, including Bitcoin.',
 	packages=[
-		'coins'
+		'coins',
 	],
 	zip_safe=False,
 	install_requires=[
 		'ecdsa>=0.10',
-		'wsgiref>=0.1.2',
 	],
 	classifiers=[
 		'Intended Audience :: Developers',
 		'License :: OSI Approved :: MIT License',
 		'Operating System :: OS Independent',
 		'Programming Language :: Python',
-	]
+	],
 )
