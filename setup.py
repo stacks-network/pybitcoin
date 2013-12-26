9224edea4cf2a13c5732537b3bcbb736f67a0b45
rxl <me@ryanshea.org>
2013-12-25 21:10:07 -0500

    update version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(
     name='Coins',
-    version='0.1.3',
+    version='0.1.4',
     url='https://github.com/halfmoonlabs/coins',
     license='MIT',
     author='Halfmoon Labs',
