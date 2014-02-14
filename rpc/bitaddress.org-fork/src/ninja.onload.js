222b96bfad256b3b1499324fb610602f3357d2d8
ibrahimahmed443 <ibrahimahmed443@gmail.com>
2014-02-14 15:28:30 +0500

    added a fork of bitaddress.org and replaced bitcoin address with namecoin address; still using the official logo though

---
+++ rpc/bitaddress.org-fork/src/ninja.onload.js
@@ -0,0 +1,26 @@
+// run unit tests
+if (ninja.getQueryString()["unittests"] == "true" || ninja.getQueryString()["unittests"] == "1") {
+	ninja.unitTests.runSynchronousTests();
+	ninja.translator.showEnglishJson();
+}
+// run async unit tests
+if (ninja.getQueryString()["asyncunittests"] == "true" || ninja.getQueryString()["asyncunittests"] == "1") {
+	ninja.unitTests.runAsynchronousTests();
+}
+// change language
+if (ninja.getQueryString()["culture"] != undefined) {
+	ninja.translator.translate(ninja.getQueryString()["culture"]);
+}
+// testnet, check if testnet edition should be activated
+if (ninja.getQueryString()["testnet"] == "true" || ninja.getQueryString()["testnet"] == "1") {
+	document.getElementById("testnet").innerHTML = ninja.translator.get("testneteditionactivated");
+	document.getElementById("testnet").style.display = "block";
+	document.getElementById("detailwifprefix").innerHTML = "'9'";
+	document.getElementById("detailcompwifprefix").innerHTML = "'c'";
+	Bitcoin.Address.networkVersion = 0x6F; // testnet
+	Bitcoin.ECKey.privateKeyPrefix = 0xEF; // testnet
+	ninja.testnetMode = true;
+}
+if (ninja.getQueryString()["showseedpool"] == "true" || ninja.getQueryString()["showseedpool"] == "1") {
+	document.getElementById("seedpoolarea").style.display = "block";
+}
