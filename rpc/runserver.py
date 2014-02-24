7d3ead1249a5377d1573e6a1cc38571c3328b9f3
Muneeb Ali <muneeb@ali.vc>
2014-02-23 20:03:25 -0500

    cleaned up code and merged common functions

--- rpc/runserver.py
+++ rpc/runserver.py
@@ -9,9 +9,12 @@
 
 import os
 from coinrpc.coinrpc import app
-from config import DEFAULT_PORT, DEFAULT_HOST, DEBUG
+from config import DEFAULT_PORT, DEFAULT_HOST, DEBUG, entered_passphrase
+import getpass
 
 def runserver():
+	#entered_passphrase = getpass.getpass('Enter passphrase: ')
+
 	port = int(os.environ.get('PORT', DEFAULT_PORT))
 	app.run(host=DEFAULT_HOST, port=port, debug=DEBUG)
 
