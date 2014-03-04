982fb0abc000fc460830a8555fc8b576d204d404
Muneeb Ali <muneeb@ali.vc>
2014-03-04 10:23:56 -0500

    separated Flask API from namecoind funtionality

--- rpc/runserver.py
+++ rpc/runserver.py
@@ -8,12 +8,11 @@
 """
 
 import os
-from coinrpc.coinrpc import app
+from coinrpc.coinrpc_flask import app
 from config import DEFAULT_PORT, DEFAULT_HOST, DEBUG, entered_passphrase
 import getpass
 
 def runserver():
-	#entered_passphrase = getpass.getpass('Enter passphrase: ')
 
 	port = int(os.environ.get('PORT', DEFAULT_PORT))
 	app.run(host=DEFAULT_HOST, port=port, debug=DEBUG)
