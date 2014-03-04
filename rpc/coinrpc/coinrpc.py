85e8d501356a116f89bf10eb3cafcc6a5656aa19
Muneeb Ali <muneeb@ali.vc>
2014-03-04 10:54:40 -0500

    new tools for registering/updating OneName users

--- rpc/coinrpc/coinrpc.py
+++ rpc/coinrpc/coinrpc.py
@@ -112,7 +112,6 @@ def check_registration(key):
         return True
 
 #-----------------------------------
-@app.route('/namecoind/validateaddress/<address>')
 def validate_address(address):
 
     reply = {}
@@ -125,22 +124,6 @@ def validate_address(address):
         reply['status'] = 404
 
     return jsonify(reply)
-#-----------------------------------
-@app.route('/namecoind/name_scan', methods = ['GET'])
-def namecoind_name_scan():
-    
-    start_name = request.args.get('start_name')     
-    if start_name == None:
-        start_name = "#"
-
-    max_returned = request.args.get('max_returned')
-    if max_returned == None:
-        max_returned = 500
-    else:
-        max_returned = int(max_returned)
-
-    info = json.dumps(namecoind.name_scan(start_name, max_returned))
-    return jsonify(info)
 
 #-----------------------------------
 #helper function for name_show
