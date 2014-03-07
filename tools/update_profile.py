91eac721e3306d682189c1228c69252fbd0f659d
Muneeb Ali <muneeb@ali.vc>
2014-03-06 19:02:50 -0500

    automated profile updating from DB or file

---
+++ tools/update_profile.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import json
+from pymongo import MongoClient
+from onename_register import update_name
+import os 
+
+MONGODB_URI = os.environ['MONGODB_URI']
+HEROKU_APP = os.environ['HEROKU_APP'] 
+remote_client = MongoClient(MONGODB_URI)
+users = remote_client[HEROKU_APP].user
+
+#-----------------------------------
+def update_profile(username,profile):
+ 
+	onename_username = 'u/' + username
+
+	print onename_username
+	print json.dumps(profile)
+	
+	#update_name does json.dumps internally
+	#update_name(onename_username,profile)
+
+#-----------------------------------
+def update_profile_from_DB(username):
+
+	entry = users.find_one({'username':username})
+	profile = json.loads(entry['profile'])
+
+	update_profile(username,profile)
+
+#-----------------------------------
+def update_profile_from_file(username,file_name='tools/json_profile.json'):
+
+	json_data=open(file_name)
+	profile = json.load(json_data)
+
+	update_profile(username,profile)
+
+#-----------------------------------
+if __name__ == '__main__':
+
+	username = 'danshipper'
+	update_profile_from_DB(username)
+	#update_profile_from_file(username)
