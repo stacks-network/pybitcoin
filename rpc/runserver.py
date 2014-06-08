111da8b518a79b40d58436fcbd9fb8fb102f9a55
Muneeb Ali <muneeb@ali.vc>
2014-06-07 22:55:10 -0700

    Added:
    -- backend identity proof checker API
    -- cleaner API structure with Blueprints
    -- construction of full_profile in the backend (by following next pointers)
    -- caching for name_show API end-points
    -- hack around profiles > 520 bytes

--- rpc/runserver.py
+++ rpc/runserver.py
@@ -8,13 +8,16 @@
 """
 
 import os
-from coinrpc.coinrpc_flask import app
+from coinrpc_api import app
 from config import *
 
+#------------------------------
 def runserver():
 
 	port = int(os.environ.get('PORT', DEFAULT_PORT))
 	app.run(host=DEFAULT_HOST, port=port, debug=DEBUG)
 
+#------------------------------
 if __name__ == '__main__':
+	
 	runserver()
