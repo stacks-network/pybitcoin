c25e84315111b7077c456be785bcea7606ab14b6
Muneeb Ali <muneeb@ali.vc>
2014-06-20 17:46:38 -0700

    small bug fix for namecoind_name_show

--- coinrpc/coinrpc.py
+++ coinrpc/coinrpc.py
@@ -170,14 +170,13 @@ def namecoind_name_show(input_key):
 
     reply = {}
 
-    max_returned = 1
-
     value = namecoind.name_show(input_key)
-    profile = value.get('value')
+    
+    profile = json.loads(value.get('value'))
 
-    #if utf8len(json.dumps(profile)) > VALUE_MAX_LIMIT:
-    #    new_key = 'i/' + input_key.lstrip('u/') + "-1"
-    #    value = namecoind.name_show(new_key)
+    if utf8len(json.dumps(profile)) > VALUE_MAX_LIMIT:
+        new_key = 'i/' + input_key.lstrip('u/') + "-1"
+        value = namecoind.name_show(new_key)   
 
     if 'code' in value and value.get('code') == -4:
         return error_reply("Not found", 404)
