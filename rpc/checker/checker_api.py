fe4b83a7356cdd50df221e70b9589491f2519c64
Muneeb Ali <muneeb@ali.vc>
2014-06-29 18:49:48 -0700

    new caching system
    small fixes

--- rpc/checker/checker_api.py
+++ rpc/checker/checker_api.py
@@ -5,7 +5,7 @@ from flask import jsonify, Blueprint, request
 from namecoin.namecoind_api import error_reply
 from namecoin.namecoind_wrapper import get_full_profile
 
-from config import *
+from config import MEMCACHED_ENABLED, MEMCACHED_PORT, MEMCACHED_TIMEOUT, DEFAULT_HOST
 
 import pylibmc
 from time import time
@@ -13,16 +13,20 @@ mc = pylibmc.Client([DEFAULT_HOST + ':' + MEMCACHED_PORT],binary=True)
 
 checker_api = Blueprint('checker_api', __name__)
 
-#-----------------------------------------
-def is_valid_proof(key, value, username):
+import hashlib 
 
-    proof_url = get_proof_url(value["proof"], username)
+#-----------------------------------------
+def is_valid_proof(key, value, username, proof_url):
+
     if "username" in value:
         site_username = value["username"]
-        if site_username not in proof_url.lower():
+        if site_username not in proof_url:
             return False
+    try:
+        r = requests.get(proof_url)
+    except:
+        return False
 
-    r = requests.get(proof_url)
     search_text = html2text(r.text)
     if key == "twitter":
         search_text = search_text.replace("<s>", "").replace("</s>", "").replace("**", "")
@@ -54,7 +58,7 @@ def get_proof_url(proof, username):
             proof_url = "https://gist.github.com/" + username + "/" + proof["id"]
         elif key == "facebook":
             proof_url = "https://www.facebook.com/" + username + "/posts/" + proof["id"]
-    return proof_url
+    return proof_url.lower()
 
 #-----------------------------------------
 @checker_api.route('/checker/get_verifications')
@@ -65,37 +69,59 @@ def get_verifications():
 
     username = request.args.get('username')
 
+    try:
+        refresh = int(request.args.get('refresh'))
+    except:
+        refresh = 0 
+
+    if refresh == 1:
+        USE_CACHE = False
+    else:
+        USE_CACHE = MEMCACHED_ENABLED
+
     if username is None:
         return error_reply("username not given")
 
-    if MEMCACHED_ENABLED: 
-        cache_reply = mc.get("proof_" + str(username))
-    else:
-        cache_reply = None
-        #print "cache off"
+    profile = get_full_profile('u/' + username)
 
-    if cache_reply is None: 
-        profile = get_full_profile('u/' + username)
+    if 'status' in profile and profile['status'] == 404:
+        headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+        url = 'http://onename.io/' + username + '.json'
+        r = requests.get(url, headers=headers)
+        profile = r.json()
 
-        if 'status' in profile and profile['status'] == 404:
-            headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-            url = 'http://onename.io/' + username + '.json'
-            r = requests.get(url, headers=headers)
-            profile = r.json()
 
-        #print profile
+    for key, value in profile.items():
+        if key in proof_sites and type(value) is dict and "proof" in value:
+        
+            try:
+                proof_username = value['username'].lower()
+                proof_url = get_proof_url(value["proof"], proof_username)
+                proof_url_hash = hashlib.md5(proof_url).hexdigest()
 
-        for key, value in profile.items():
-            if key in proof_sites and type(value) is dict and "proof" in value:
+            except:
+                continue 
 
-                if is_valid_proof(key, value, username):
+            #if user is trying to put some other link
+            if proof_username not in proof_url:
+                continue
+
+            if USE_CACHE: 
+                cache_reply = mc.get("proof_" + proof_url_hash)
+            else:
+                cache_reply = None 
+                #print "cache off"
+
+            if cache_reply is None: 
+
+                if is_valid_proof(key, value, username, proof_url):
                     verifications[key] = True
     
-        if MEMCACHED_ENABLED:
-            mc.set("proof_" + str(username),json.dumps(verifications),int(time() + MEMCACHED_TIMEOUT))
-            #print "cache miss"
-    else:
-        #print "cache hit"
-        verifications = json.loads(cache_reply)
+                    if USE_CACHE:
+                        mc.set("proof_" + proof_url_hash,True,int(time() + MEMCACHED_TIMEOUT))
+                        #print "cache miss"
+            else:
+                #print "cache hit"
+                verifications[key] = cache_reply
 
     return jsonify(verifications)
