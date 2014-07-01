137350e1089748bb367499c4ab8569e5cbf6b4c9
ibrahimahmed443 <ibrahimahmed443@gmail.com>
2014-07-01 18:11:16 +0500

    added a few calls from the bitcoin-rpc and created a Flask wrapper

---
+++ rpc/bitcoin/config.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+#-----------------------
+# Copyright 2013 Halfmoon Labs, Inc.
+# All Rights Reserved
+#-----------------------
+
+'''
+    configuration file for bitcoind_api
+'''
+
+DEBUG = True
+
+DEFAULT_PORT = 5000
+DEFAULT_HOST = '127.0.0.1'
+
+BITCOIND_PORT = 5000
+BITCOIND_SERVER = 'bitd.halfmoonlabs.com'
+BITCOIND_USER = 'halfmoon4534t4csdv3ry3rtwcdsv2t'
+BITCOIND_PASSWD = 'gfdhknr2gn34trdgve4g3vcv29evxcvcfhgnnsvsnstotalcare4353tfish'
+BITCOIND_USE_HTTPS = False 
