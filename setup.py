2b528eb09de7919ee3bcf0c4dcec6e5a7d8e67b6
rxl <me@ryanshea.org>
2014-09-26 10:35:20 -0400

    update version number

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(
     name='coinkit',
-    version='0.5.1',
+    version='0.5.2',
     url='https://github.com/onenameio/coinkit',
     license='MIT',
     author='Onename (Halfmoon Labs)',
