db3f162496f1443242de9650e1fb815c9b7f338e
Ryan Shea <ryan@shea.io>
2014-10-07 15:50:44 -0400

    update version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(
     name='coinkit',
-    version='0.5.2',
+    version='0.5.3',
     url='https://github.com/onenameio/coinkit',
     license='MIT',
     author='Onename (Halfmoon Labs)',
