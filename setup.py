be2ead9d55aadfa8314b28b2e8ae43819ca5d9a2
Muneeb Ali <muneeb@ali.vc>
2015-04-06 15:53:33 -0400

    reorganized as module inside coinkit/ dir plus updates to setup/requirements

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ setup(
     packages=find_packages(),
     zip_safe=False,
     install_requires=[
+        'commontools==0.1.0',
         'ecdsa==0.11',
         'utilitybelt>=0.2.1',
         'requests>=2.4.3',
