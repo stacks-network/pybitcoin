65121a7a0ea1f97b1f22b0b2bae772e4d4a378bb
Muneeb Ali <muneeb@ali.vc>
2015-11-23 17:38:26 -0500

    updated version of keychain

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
         'utilitybelt>=0.2.1',
         'pybitcointools==1.1.15',
         'python-bitcoinrpc==0.1',
-        'keychain>=0.1.0',
+        'keychain>=0.1.4',
         'bitcoin>=1.1.39'
     ],
     classifiers=[
