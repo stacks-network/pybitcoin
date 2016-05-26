78979fb2054338e2a68ca1bf0ece6753af3ee21c
Jude Nelson <judecn@gmail.com>
2016-05-26 17:11:16 -0400

    utilitybelt 0.2.6

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
         'requests>=2.4.3',
         'ecdsa>=0.13',
         'commontools==0.1.0',
-        'utilitybelt>=0.2.1',
+        'utilitybelt>=0.2.6',
         'python-bitcoinrpc==0.1',
         'keychain>=0.1.4',
         'bitcoin>=1.1.42'
