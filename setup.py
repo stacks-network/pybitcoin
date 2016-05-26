4b35cec133087d97ca83cd7fa614e5dce72f5c1c
Jude Nelson <judecn@gmail.com>
2016-05-26 16:47:36 -0400

    bitcoin 1.1.42

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 """
 pybitcoin
 ==============
@@ -24,7 +25,7 @@ setup(
         'utilitybelt>=0.2.1',
         'python-bitcoinrpc==0.1',
         'keychain>=0.1.4',
-        'bitcoin>=1.1.39'
+        'bitcoin>=1.1.42'
     ],
     classifiers=[
         'Intended Audience :: Developers',
